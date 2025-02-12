5d5c53262ca5a05a4ed4927ad58813119ee3585d
ultrs <filatovd5421@gmail.com>
2025-02-12 23:17:13 +0300

    12022025

--- src/com/mentoring/sc/cyclePractice/Task2.java
+++ src/com/mentoring/sc/cyclePractice/Task2.java
@@ -1,4 +1,5 @@
 package com.mentoring.sc.cyclePractice;
+
 import java.util.Scanner;
 /*
 Написать программу, вычисляющую и отображающую все числа Фибоначчи меньше введённого пользователем целого числа.
@@ -11,19 +12,21 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input: ");
-        int threshold = scanner.nextInt();
-
+        int marker = scanner.nextInt();
+        fibonachchiPrinter(marker);
     }
 
-    public static void fibonachchiPrinter(int end){
-//        int first = 0;
-//        int second = 1;
-//        while(start <= end){
-//            if(){}
-//        }
+    public static void fibonachchiPrinter(int end) {
+        int number = 0;
+        int currentNumber = 1;
+        while (currentNumber + number < end) {
+            currentNumber = currentNumber + number;
+            number = currentNumber - number;
+            printer(currentNumber);
+        }
     }
 
-    public static void printer(int value){
-        System.out.println(value);
+    public static void printer(int value) {
+        System.out.print(value + "  ");
     }
 }
--- src/com/mentoring/sc/cyclePractice/Task3.java
+++ src/com/mentoring/sc/cyclePractice/Task3.java
@@ -1,4 +1,54 @@
 package com.mentoring.sc.cyclePractice;
 
+/*
+Написать программу, отображающую статистику по осадкам за N дней. N вводится пользователем.
+Пользователь также должен ввести N целых чисел, обозначающих величину осадков в день.
+
+Программа должна выводить:
+
+a. Количество дней
+b. Сумму осадков за этот период
+c. Среднее количество осадков за этот период
+d. Максимальное количество дневных осадков за этот период
+ */
+
+import java.util.Scanner;
+
 public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of days: ");
+        int days = scanner.nextInt();
+        int[] precipitationDay = new int[days];
+        for (int i = 0; i < days; i++) {
+            System.out.println("Enter the amount of precipitation: ");
+            precipitationDay[i] = scanner.nextInt();
+        }
+        result(days, precipitationDay);
+    }
+
+    private static void result(int days, int[] precipitation) {
+        System.out.println("Days: " + days);
+        System.out.println("Sum of precipitation: " + sumOfPrecipitation(precipitation));
+        System.out.println("Average precipitation: " + sumOfPrecipitation(precipitation) / days);
+        System.out.println("Max precipitation: " + maxOfPrecipitation(precipitation));
+    }
+
+    private static int sumOfPrecipitation(int[] precipitation) {
+        int sum = 0;
+        for (int element : precipitation) {
+            sum += element;
+        }
+        return sum;
+    }
+
+    private static int maxOfPrecipitation(int[] precipitation) {
+        int maxValue = 0;
+        for (int element : precipitation) {
+            if (maxValue < element) maxValue = element;
+        }
+        return maxValue;
+    }
+
 }
